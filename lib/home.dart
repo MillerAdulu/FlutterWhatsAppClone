0569c4f6f3c2046d52312cd6be6356d83c04b17f
Miller Adulu <milleradulu@gmail.com>
2020-11-21 16:40:52 +0300

    Clear app

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutterwhatsapp/pages/call_screen.dart';
-import 'package:flutterwhatsapp/pages/chat_screen.dart';
-import 'package:flutterwhatsapp/pages/status_screen.dart';
 
 class WhatsAppHome extends StatefulWidget {
   @override
@@ -10,68 +7,8 @@ class WhatsAppHome extends StatefulWidget {
 
 class _WhatsAppHomeState extends State<WhatsAppHome>
     with SingleTickerProviderStateMixin {
-  TabController _tabController;
-  bool showFab = true;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _tabController = TabController(
-      vsync: this,
-      initialIndex: 1,
-      length: 3,
-    );
-
-    _tabController.addListener(() {
-      if (_tabController.index == 0) {
-        showFab = true;
-      } else {
-        showFab = false;
-      }
-      setState(() {});
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("WhatsApp"),
-        elevation: 0.7,
-        bottom: TabBar(
-          controller: _tabController,
-          indicatorColor: Colors.white,
-          tabs: <Widget>[
-            Tab(text: "CHATS"),
-            Tab(text: "STATUS"),
-            Tab(text: "CALLS"),
-          ],
-        ),
-        actions: <Widget>[
-          Icon(Icons.search),
-          Padding(padding: const EdgeInsets.symmetric(horizontal: 5.0)),
-          Icon(Icons.more_vert)
-        ],
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: <Widget>[
-          ChatScreen(),
-          StatusScreen(),
-          CallsScreen(),
-        ],
-      ),
-      floatingActionButton: showFab
-          ? FloatingActionButton(
-              backgroundColor: Theme.of(context).accentColor,
-              child: Icon(
-                Icons.message,
-                color: Colors.white,
-              ),
-              onPressed: () => print("New chat"),
-            )
-          : null,
-    );
+    return Scaffold(body: Center(child: Text("Welcome to WhatsApp")));
   }
 }
--- lib/pages/status_screen.dart
+++ lib/pages/status_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutterwhatsapp/pages/store_page_view.dart';
 
 class StatusScreen extends StatelessWidget {
   @override
@@ -9,83 +8,7 @@ class StatusScreen extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Card(
-            color: Colors.white,
-            elevation: 0.0,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListTile(
-                leading: Stack(
-                  children: <Widget>[
-                    CircleAvatar(
-                      radius: 30,
-                      backgroundImage: NetworkImage(
-                        "https://picsum.photos/100?random=8",
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 0.0,
-                      right: 1.0,
-                      child: Container(
-                        height: 20,
-                        width: 20,
-                        child: Icon(
-                          Icons.add,
-                          color: Colors.white,
-                          size: 15,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.green,
-                          shape: BoxShape.circle,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                title: Text(
-                  "My Status",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                subtitle: Text("Tap to add status update"),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "Viewed updates",
-              style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(8.0),
-              color: Colors.white,
-              child: ListView(
-                children: <Widget>[
-                  ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      backgroundImage: NetworkImage(
-                        "https://picsum.photos/100?random=9",
-                      ),
-                    ),
-                    title: Text(
-                      "Developer Circles",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text("Today, 20:16 PM"),
-                    onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => StoryPageView(),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          Text("Welcome to statuses"),
         ],
       ),
     );
